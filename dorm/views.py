0871ad4eeb5bb37e86c485cd663b8455dc4a8e62
Joey <t1616joy@yahoo.com.tw>
2022-05-01 15:56:12 +0800

    present data by table

--- dorm/views.py
+++ dorm/views.py
@@ -29,20 +29,38 @@ def post(request):
 
 # def get(request,id):
 def get(request):
-    name=[]
-    info=[]
-    voltage=[]
-    Date=[]
-    for i in range(1,3): #3 is tricky
-        facilities_list= facilities.objects.get(id=i)
-        Date.append(facilities_list.Date)
-        name.append(facilities_list.facilities_name)
-        info.append(facilities_list.facilities_info)
-        voltage.append(facilities_list.facilities_voltage)
-    context = {
-        'Date':Date,
-        'facilities_list': name,
-        'facilities_info': info,
-        'facilities_voltage':voltage
-    }
-    return render(request,"dorm.html",context)
+
+    context = facilities.objects.all()
+    # name=[]
+    # info=[]
+    # voltage=[]
+    # Date=[]
+    # tmp=[]
+    # for i in range(2,3): #3 is tricky
+    #     # facilities_washing=facilities.objects.filter(facilities_name="washing machine")
+    #     # tmp.append(facilities_washing[0])
+    #     facilities_washing= facilities.objects.get(facilities_name="washing machine") # dry machine # toilet
+    #     Date.append(facilities_washing.Date)
+    #     name.append(facilities_washing.facilities_name)
+    #     info.append(facilities_washing.facilities_info)
+    #     voltage.append(facilities_washing.facilities_voltage)
+
+    #     facilities_dry= facilities.objects.get(facilities_name="dry machine") # washing machine # toilet
+    #     Date.append(facilities_dry.Date)
+    #     name.append(facilities_dry.facilities_name)
+    #     info.append(facilities_dry.facilities_info)
+    #     voltage.append(facilities_dry.facilities_voltage)
+
+    #     facilities_toilet= facilities.objects.get(facilities_name="toilet") # washing machine # toilet
+    #     Date.append(facilities_toilet.Date)
+    #     name.append(facilities_toilet.facilities_name)
+    #     info.append(facilities_toilet.facilities_info)
+    #     voltage.append(facilities_toilet.facilities_voltage)
+    # context = {
+    #     # 'tmp':tmp,
+    #     'Date':Date,
+    #     'facilities_list': name,
+    #     'facilities_info': info,
+    #     'facilities_voltage':voltage
+    # }
+    return render(request,"dorm.html",{"context":context})
