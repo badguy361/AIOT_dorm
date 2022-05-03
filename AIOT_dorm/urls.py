148c14f0214bcdbfdfcd82663deb7ed9637bc363
Joey <t1616joy@yahoo.com.tw>
2022-05-03 13:55:10 +0800

    add static url

--- AIOT_dorm/urls.py
+++ AIOT_dorm/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from dorm.views import post
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [ 
     path('postdorm/', post),
     # path('putdorm/', put),
     path('dorm/',include('dorm.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
