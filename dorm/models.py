ac73f0b0e5913580a2896fe2b9a772b6016202ef
Joey <t1616joy@yahoo.com.tw>
2022-05-14 22:12:37 +0800

    refresh model

--- dorm/models.py
+++ dorm/models.py
@@ -2,9 +2,18 @@ from django.db import models
 
 # Create your models here.
 class facilities(models.Model):
-	Date = models.IntegerField()
+	Date = models.CharField(max_length = 30)
 	facilities_name = models.CharField(max_length = 20)
-	facilities_info = models.CharField(max_length = 10)
-	facilities_voltage = models.FloatField()
+	facilities_useage = models.CharField(max_length = 10)
+	battery = models.FloatField()
+	Acce_X = models.FloatField()
+	Acce_Y = models.FloatField()
+	Acce_Z = models.FloatField()
+	Acce = models.FloatField()
+	Temp = models.FloatField()
+	Current = models.FloatField()
+	Voltage = models.FloatField()
+	Power = models.FloatField()
+
 	def __str__(self):
 		return self.facilities_name
--- dorm/static/javascript/dorm.js
+++ dorm/static/javascript/dorm.js
@@ -8,7 +8,6 @@ const jsondata = {
 };
 
 async function myAsyncFunction(){
-    //Do what you want here 
     while(1){
         console.log("start to fetch data")
         fetch('http://140.115.200.38:80/dorm/example')
@@ -16,14 +15,12 @@ async function myAsyncFunction(){
                 return response.json();
             })
             .then((data) => {
-                // console.log(data.Date)
                 jsondata.Date = data.Date
                 console.log(jsondata)
+            })
+            .catch((error) => {
+                console.log(`Error: ${error}`);
             });
-            // .catch((error) => {
-            //     console.log(`Error: ${error}`);
-            // })
-            // mydata.Date = jsondata
 
         await delay(10);
         }
