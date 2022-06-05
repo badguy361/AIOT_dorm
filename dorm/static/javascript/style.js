f78eeaa775b4fbe0ca10f77f02e25d5888640cf8
Joey <t1616joy@yahoo.com.tw>
2022-06-05 16:31:43 +0800

    add animation

--- dorm/static/javascript/style.js
+++ dorm/static/javascript/style.js
@@ -1,37 +1,72 @@
-function delay(n){
-    return new Promise(function(resolve){
-        setTimeout(resolve,n*1000);
+function delay(n) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, n * 1000);
     });
 }
 
-async function myAsyncFunction(){
-    while(1){
-        const washingMachine = document.getElementById('wash-box-1');
+async function myAsyncFunction() {
+    while (1) {
+        const washingMachine_box1 = document.getElementById('wash-box-1');
+        const washingMachine_1 = document.getElementById('washingMachine-1');
+        const shower_box1 = document.getElementById('bath-box-1');
+        const shower_1 = document.getElementById('shower-1');
+        const disabled_box1 = document.getElementById('dis-box-1');
+        const disabled_1 = document.getElementById('disabled-1');
+        const dryerMachine_box1 = document.getElementById('dry-box-1');
+        const dryerMachine_1 = document.getElementById('dryerMachine-1');
 
         console.log("start to fetch data");
         fetch('http://140.115.200.38:80/dorm/getJsondata')
             .then((response) => {
-                console.log("response.json()",response)
+                console.log("response.json()", response)
                 return response.json();
             })
-            .then((data) => { 
-                washingMachine_battery = data[2]["fields"]['battery']
-                console.log(washingMachine_battery)
-                
-                if (parseInt(washingMachine_battery) > 10){
-                    washingMachine.style.background= 'yellow'
-                }else{
-                    washingMachine.style.background= '#609755'
+            .then((data) => {
+                washingMachine_box1_usage = data[2]["fields"]['battery']
+                washingMachine_1_usage = data[2]["fields"]['battery']
+                shower_box1_usage = data[2]["fields"]['battery']
+                shower_1_usage = data[2]["fields"]['battery']
+                disabled_box1_usage = data[2]["fields"]['battery']
+                disabled_1_usage = data[2]["fields"]['battery']
+                dryerMachine_box1_usage = data[2]["fields"]['battery']
+                dryerMachine_1_usage = data[2]["fields"]['battery']
+                console.log(washingMachine_1_usage)
+
+                if (parseInt(washingMachine_box1_usage) > 10) {
+                    washingMachine_box1.style.background = 'yellow';
+                    washingMachine_1.style.fill = '#4071F5';
+                } else {
+                    washingMachine_box1.style.background = '#609755';
                 }
 
+                if (parseInt(shower_1_usage) > 10) {
+                    shower_1.style.opacity = 1;
+                    shower_box1.style.background = 'yellow';
+                } else {
+                    shower_box1.style.background = '#609755';
+                }
+
+                if (parseInt(disabled_1_usage) > 10) {
+                    disabled_1.style.opacity = 1;
+                    disabled_box1.style.background = 'yellow';
+                } else {
+                    disabled_box1.style.background = '#609755';
+                }
+
+                if (parseInt(dryerMachine_1_usage) > 10) {
+                    dryerMachine_1.style.animation = 'dash 1.4s linear infinite';
+                    dryerMachine_box1.style.background = 'yellow';
+                } else {
+                    dryerMachine_box1.style.background = '#609755';
+                }
             })
             .catch((error) => {
                 console.log(`Error: ${error}`);
             });
-        
-            
+
+
         await delay(10);
-        }
+    }
 }
 
 myAsyncFunction();
