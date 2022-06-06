d98276374a3d677d8e0a909fb195f02ceb73c1d4
Joey <t1616joy@yahoo.com.tw>
2022-06-06 14:46:30 +0800

    tmp

--- dorm/static/javascript/style.js
+++ dorm/static/javascript/style.js
@@ -18,24 +18,29 @@ async function myAsyncFunction() {
         console.log("start to fetch data");
         fetch('http://140.115.200.38:80/dorm/getJsondata')
             .then((response) => {
-                console.log("response.json()", response)
+                console.log("response.json()", response);
                 return response.json();
             })
             .then((data) => {
-                washingMachine_box1_usage = data[2]["fields"]['battery']
-                washingMachine_1_usage = data[2]["fields"]['battery']
-                shower_box1_usage = data[2]["fields"]['battery']
-                shower_1_usage = data[2]["fields"]['battery']
-                disabled_box1_usage = data[2]["fields"]['battery']
-                disabled_1_usage = data[2]["fields"]['battery']
-                dryerMachine_box1_usage = data[2]["fields"]['battery']
-                dryerMachine_1_usage = data[2]["fields"]['battery']
-                console.log(washingMachine_1_usage)
+                washingMachine_box1_usage = data[0]["fields"]['battery'];
+                washingMachine_1_usage = data[0]["fields"]['battery'];
+                shower_box1_usage = data[1]["fields"]['battery'];
+                shower_1_usage = data[1]["fields"]['battery'];
+                disabled_box1_usage = data[2]["fields"]['battery'];
+                disabled_1_usage = data[2]["fields"]['battery'];
+                dryerMachine_box1_usage = data[3]["fields"]['battery'];
+                dryerMachine_1_usage = data[3]["fields"]['battery'];
+                
+                console.log("washingMachine_1_usage",washingMachine_1_usage);
+                console.log(shower_1_usage);
+                console.log(disabled_1_usage);
+                console.log(dryerMachine_1_usage);
 
                 if (parseInt(washingMachine_box1_usage) > 10) {
                     washingMachine_box1.style.background = 'yellow';
                     washingMachine_1.style.fill = '#4071F5';
                 } else {
+                    washingMachine_1.style.fill = 'white';
                     washingMachine_box1.style.background = '#609755';
                 }
 
@@ -43,6 +48,7 @@ async function myAsyncFunction() {
                     shower_1.style.opacity = 1;
                     shower_box1.style.background = 'yellow';
                 } else {
+                    shower_1.style.opacity = 0.1;
                     shower_box1.style.background = '#609755';
                 }
 
@@ -50,6 +56,7 @@ async function myAsyncFunction() {
                     disabled_1.style.opacity = 1;
                     disabled_box1.style.background = 'yellow';
                 } else {
+                    disabled_1.style.opacity = 0.1;
                     disabled_box1.style.background = '#609755';
                 }
 
