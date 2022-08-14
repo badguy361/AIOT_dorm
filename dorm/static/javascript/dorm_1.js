919e72521c6f106908a6b72d832a32bc84d49849
Henry Chin <48316662+pikaiscoming@users.noreply.github.com>
2022-08-14 21:06:14 +0800

    區別樓層變數

--- dorm/static/javascript/dorm_1.js
+++ dorm/static/javascript/dorm_1.js
@@ -6,30 +6,30 @@ function delay(n) {
 
 async function myAsyncFunction() {
     while (1) {
-        const washingMachine_box1 = document.getElementById('wash-box-1-1');
-        const washingMachine_1 = document.getElementById('washingMachine-1-1');
-        const washingMachine_box2 = document.getElementById('wash-box-1-2');
-        const washingMachine_2 = document.getElementById('washingMachine-1-2');
+      const f1washingMachine_box1 = document.getElementById('wash-box-1-1');
+      const f1washingMachine_1 = document.getElementById('washingMachine-1-1');
+      const f1washingMachine_box2 = document.getElementById('wash-box-1-2');
+      const f1washingMachine_2 = document.getElementById('washingMachine-1-2');
 
-        const shower_box1 = document.getElementById('bath-box-1-1');
-    		const shower_1 = document.getElementById('bath-1-1');
-    		const shower_box2 = document.getElementById('bath-box-1-2');
-    		const shower_2 = document.getElementById('bath-1-2');
-    		const shower_box3 = document.getElementById('bath-box-1-3');
-    		const shower_3 = document.getElementById('bath-1-3');
-    		const shower_box4 = document.getElementById('bath-box-1-4');
-    		const shower_4 = document.getElementById('bath-1-4');
-    		const shower_box5 = document.getElementById('bath-box-1-5');
-    		const shower_5 = document.getElementById('bath-1-5');
+      const f1shower_box1 = document.getElementById('bath-box-1-1');
+      const f1shower_1 = document.getElementById('bath-1-1');
+      const f1shower_box2 = document.getElementById('bath-box-1-2');
+      const f1shower_2 = document.getElementById('bath-1-2');
+      const f1shower_box3 = document.getElementById('bath-box-1-3');
+      const f1shower_3 = document.getElementById('bath-1-3');
+      const f1shower_box4 = document.getElementById('bath-box-1-4');
+      const f1shower_4 = document.getElementById('bath-1-4');
+      const f1shower_box5 = document.getElementById('bath-box-1-5');
+      const f1shower_5 = document.getElementById('bath-1-5');
 
-        const disabled_box1 = document.getElementById('dis-box-1-1');
-        const disabled_1 = document.getElementById('disabled-1-1');
-        const dryerMachine_box1 = document.getElementById('dry-box-1-1');
-        const dryerMachine_1 = document.getElementById('dryerMachine-1-1');
+      const f1disabled_box1 = document.getElementById('dis-box-1-1');
+      const f1disabled_1 = document.getElementById('disabled-1-1');
+      const f1dryerMachine_box1 = document.getElementById('dry-box-1-1');
+      const f1dryerMachine_1 = document.getElementById('dryerMachine-1-1');
 
-        const wc_1 = document.getElementById('wc-1-1');
-        const wc_2 = document.getElementById('wc-1-2');
-        const wc_3 = document.getElementById('wc-1-3');
+      const f1wc_1 = document.getElementById('wc-1-1');
+      const f1wc_2 = document.getElementById('wc-1-2');
+      const f1wc_3 = document.getElementById('wc-1-3');
 
 
         console.log("start to fetch data");
@@ -55,89 +55,89 @@ async function myAsyncFunction() {
                 console.log(dryerMachine_1_usage);
 
                 //下面的顏色動畫確定可以
-            		if (parseInt(washingMachine_box1_usage) > 10) {
-            				washingMachine_box1.style.background = 'yellow';
-            				washingMachine_1.style.fill = '#4071F5';
-            		} else {
-            				washingMachine_1.style.fill = '#faf4f1';
-            				washingMachine_box1.style.background = '#609755';
-            		}
-
-            		if (parseInt(washingMachine_box2_usage) > 10) {
-            				washingMachine_box2.style.background = 'yellow';
-            				washingMachine_2.style.fill = '#4071F5';
-            		} else {
-            				washingMachine_2.style.fill = '#faf4f1';
-            				washingMachine_box2.style.background = '#609755';
-            		}
-
-                if (parseInt(shower_1_usage) > 10) {
-            				shower_1.style.opacity = 1;
-            				shower_box1.style.background = 'yellow';
-            		} else {
-            				shower_1.style.opacity = 0.1;
-            				shower_box1.style.background = '#609755';
-            		}
-
-            		if (parseInt(shower_2_usage) > 10) {
-            				shower_2.style.opacity = 1;
-            				shower_box2.style.background = 'yellow';
-            		} else {
-            				shower_2.style.opacity = 0.1;
-            				shower_box2.style.background = '#609755';
-            		}
-
-            		if (parseInt(shower_3_usage) > 10) {
-            				shower_3.style.opacity = 1;
-            				shower_box3.style.background = 'yellow';
-            		} else {
-            				shower_3.style.opacity = 0.1;
-            				shower_box3.style.background = '#609755';
-            		}
-            		if (parseInt(shower_4_usage) > 10) {
-            				shower_4.style.opacity = 1;
-            				shower_box4.style.background = 'yellow';
-            		} else {
-            				shower_4.style.opacity = 0.1;
-            				shower_box4.style.background = '#609755';
-            		}
-            		if (parseInt(shower_5_usage) > 10) {
-            				shower_5.style.opacity = 1;
-            				shower_box5.style.background = 'yellow';
-            		} else {
-            				shower_5.style.opacity = 0.1;
-            				shower_box5.style.background = '#609755';
-            		}
-
-                if (parseInt(disabled_1_usage) > 10) {
-                    disabled_1.style.opacity = 1;
-                    disabled_box1.style.background = 'yellow';
+                if (parseInt(f1washingMachine_box1_usage) > 10) {
+                    f1washingMachine_box1.style.background = 'yellow';
+                    f1washingMachine_1.style.fill = '#4071F5';
                 } else {
-                    disabled_1.style.opacity = 0.1;
-                    disabled_box1.style.background = '#609755';
+                    f1washingMachine_1.style.fill = '#faf4f1';
+                    f1washingMachine_box1.style.background = '#609755';
                 }
 
-                if (parseInt(dryerMachine_1_usage) > 10) {
-                    dryerMachine_1.style.animation = 'dash 1.4s linear infinite';
-                    dryerMachine_box1.style.background = 'yellow';
+                if (parseInt(f1washingMachine_box2_usage) > 10) {
+                    f1washingMachine_box2.style.background = 'yellow';
+                    f1washingMachine_2.style.fill = '#4071F5';
                 } else {
-                    dryerMachine_box1.style.background = '#609755';
+                    f1washingMachine_2.style.fill = '#faf4f1';
+                    f1washingMachine_box2.style.background = '#609755';
                 }
 
-                if (parseInt(wc_1_usage) > 10) {
-                    wc_1.style.opacity = 0.8;
+                if (parseInt(f1shower_1_usage) > 10) {
+                    f1shower_1.style.opacity = 1;
+                    f1shower_box1.style.background = 'yellow';
                 } else {
-                    wc_1.style.opacity = 0.1;
+                    f1shower_1.style.opacity = 0.1;
+                    f1shower_box1.style.background = '#609755';
                 }
-                if (parseInt(wc_2_usage) > 10) {
-                    wc_2.style.opacity = 0.8;
+
+                if (parseInt(f1shower_2_usage) > 10) {
+                    f1shower_2.style.opacity = 1;
+                    f1shower_box2.style.background = 'yellow';
                 } else {
-                    wc_2.style.opacity = 0.1;
+                    f1shower_2.style.opacity = 0.1;
+                    f1shower_box2.style.background = '#609755';
                 }
-                if (parseInt(wc_3_usage) > 10) {
-                    wc_3.style.opacity = 0.8;
+
+                if (parseInt(f1shower_3_usage) > 10) {
+                    f1shower_3.style.opacity = 1;
+                    f1shower_box3.style.background = 'yellow';
                 } else {
-                    wc_3.style.opacity = 0.1;
+                    f1shower_3.style.opacity = 0.1;
+                    f1shower_box3.style.background = '#609755';
+                }
+                if (parseInt(f1shower_4_usage) > 10) {
+                    f1shower_4.style.opacity = 1;
+                    f1shower_box4.style.background = 'yellow';
+                } else {
+                    f1shower_4.style.opacity = 0.1;
+                    f1shower_box4.style.background = '#609755';
+                }
+                if (parseInt(f1shower_5_usage) > 10) {
+                    f1shower_5.style.opacity = 1;
+                    f1shower_box5.style.background = 'yellow';
+                } else {
+                    f1shower_5.style.opacity = 0.1;
+                    f1shower_box5.style.background = '#609755';
+                }
+
+                if (parseInt(f1disabled_1_usage) > 10) {
+                    f1disabled_1.style.opacity = 1;
+                    f1disabled_box1.style.background = 'yellow';
+                } else {
+                    f1disabled_1.style.opacity = 0.1;
+                    f1disabled_box1.style.background = '#609755';
+                }
+
+                if (parseInt(f1dryerMachine_1_usage) > 10) {
+                    f1dryerMachine_1.style.animation = 'dash 1.4s linear infinite';
+                    f1dryerMachine_box1.style.background = 'yellow';
+                } else {
+                    f1dryerMachine_box1.style.background = '#609755';
+                }
+
+                if (parseInt(f1wc_1_usage) > 10) {
+                    f1wc_1.style.opacity = 0.8;
+                } else {
+                    f1wc_1.style.opacity = 0.1;
+                }
+                if (parseInt(f1wc_2_usage) > 10) {
+                    f1wc_2.style.opacity = 0.8;
+                } else {
+                    f1wc_2.style.opacity = 0.1;
+                }
+                if (parseInt(f1wc_3_usage) > 10) {
+                    f1wc_3.style.opacity = 0.8;
+                } else {
+                    f1wc_3.style.opacity = 0.1;
                 }
             })
             .catch((error) => {
